5f0e67dcc2e3e36fa0d711abf1a517d134db3671
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2024-05-20 23:01:28 +0800

    feat(main.jsx) : Create Router Component to handle routing in client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home.jsx";
+import Login from "./routes/Login.jsx";
+import Regis from "./routes/Regis.jsx";
+import ComunityPage from "./routes/ComunityPage.jsx";
+import BlogPage from "./routes/BlogPage.jsx";
+import ArticlePage from "./routes/ArticlePage.jsx";
+import Calender from "./routes/Calender.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/regis",
+    element: <Regis />,
+  },
+  {
+    path: "/komunitas",
+    element: <ComunityPage />,
+  },
+  {
+    path: "/blog",
+    element: <BlogPage />,
+  },
+  {
+    path: "/artikel",
+    element: <ArticlePage />,
+  },
+  {
+    path: "/kalender",
+    element: <Calender />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}/> 
+  </React.StrictMode>
+);
