ce620f19ced9d24682cbeb0e74b886d8d17a036f
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2024-05-21 19:46:24 +0800

    feat(Home) : Adding MVP Preview section

--- client/src/routes/Home/Home.jsx
+++ client/src/routes/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./Home.module.css";
 import HeaderNavbar from "../../components/HeaderNavbar/HeaderNavbar";
+import articleMVP from "../../assets/homepage-article.svg";
+import komunitasMVP from "../../assets/homepage-komunitas.svg"
 
 
 export default function Home() {
@@ -16,15 +18,29 @@ export default function Home() {
       </header>
 
       {/* Show MVP section */}
-      <section>
+      <section className={styles.mvp_preview}>
         {/* for article */}
-        <article></article>
+        <article className={styles.article}>
+        <img src={articleMVP} alt="Article" />
+          <div className={styles.mvp_desc}>
+            <h3>Artikel Kebudayaan Yogyakarta</h3>
+            <p>Telusuri pesona budaya Yogyakarta melalui artikel kami yang mendalam. Temukan cerita-cerita menarik tentang tradisi, seni, dan sejarah yang membentuk identitas unik kota ini. Dari seni wayang hingga keindahan arsitektur kraton, jelajahi dan pahami kekayaan budaya istimewa Yogyakarta.</p>
+            <div className={styles.button}>Selengkapnya</div>
+          </div>
+        </article>
         {/* for komunitas */}
-        <article></article>
+        <article className={styles.komunitas}>
+          <div className={styles.mvp_desc}>
+            <h3>Komunitas Seni Pertunjukan</h3>
+            <p className={styles.komunitas_desc}>Temukan komunitas kebudayaan Yogyakarta dan terhubunglah dengan para pecinta budaya lokal. Teman-teman baru dan diskusi yang mendalam tentang seni, tradisi, dan acara budaya. Jadilah bagian dari upaya melestarikan dan memperkenalkan kekayaan budaya istimewa Yogyakarta kepada dunia.</p>
+            <div className={styles.button}>Selengkapnya</div>
+          </div>
+          <img src={komunitasMVP} alt="Article" />
+        </article>
       </section>
 
       {/* Populer Article */}
-      <section></section>
+      <section className={styles}></section>
 
       {/* Blog Preview  */}
       <section></section>
