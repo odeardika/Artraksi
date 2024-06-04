f0df20a650f55254cc9a110f99b2d159b241409e
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2024-06-04 23:33:36 +0800

    fix : Footer Component

--- client/src/components/FooterComponent/FooterComponent.jsx
+++ client/src/components/FooterComponent/FooterComponent.jsx
@@ -1,15 +1,15 @@
-
 import { Container, Row, Col } from "react-bootstrap";
-import logo from '../assets/img/Logo.svg';
+import logo from '../../assets/logo/Logo.svg';
+import './FooterComponent.css';
 
 const FooterComponent = () => {
   return (
-    <footer className="footer w-100">
+    <footer className="footer">
       <Container className="footer-content">
         <Row>
           <Col>
-            <img src={logo} alt="Logo" />
-            <p className="footer-text"> Lestarikan Seni Melalui Digitalisasi </p>
+            <img src={logo} className="footer-logo" alt="Logo" />
+            <p className="footer-text"> "Lestarikan Seni Melalui Digitalisasi" </p>
             <ul className="menus">
               <li><a href="#">Beranda</a></li>
               <li><a href="#">Artikel</a></li>
