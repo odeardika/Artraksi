8ac2dd74b4cb1d53be29731be5a967ce1be66f3b
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2024-06-05 17:48:16 +0800

    feat (Home): get article from database

--- client/src/components/ArticleCard/ArticleCard.jsx
+++ client/src/components/ArticleCard/ArticleCard.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import styles from './ArticleCard.module.css';
-import img from './../../assets/article/0.png';
-import userIcon from '../../assets/icon/Avatar.svg';
 
-export default function ArticleCard() {
+export default function ArticleCard({props}) {
   return (
     <>
     <div>
         <div className={styles.img_container}>
-            <img src={img} alt="" />
+            <img src={`${import.meta.env.VITE_SERVER_URL}/${props.thumbnail_img}`} alt="" />
         </div>
         <div className={styles.author_detail}>
-            <img src={userIcon} className={styles.author_img} alt="" />
-            <p className={styles.author_text}>By Tim Artraksi  | 22 jam lalu</p>
+            <img src={`${import.meta.env.VITE_SERVER_URL}/${props.creator_profile}`} className={styles.author_img} alt="" />
+            <p className={styles.author_text}>By {props.creator_name}  | {props.upload_date}</p>
         </div>
         <div className={styles.article_detail}>
-            <h3>Tari Bedhaya: Keanggunan Sakral dari Keraton Yogyakarta</h3>
-            <p>Tari Bedhaya adalah salah satu bentuk tarian klasik Jawa yang berasal dari Keraton...</p>
+            <h3>{props.title}</h3>
+            <p>{props.preview}</p>
             <div>Selengkapnya <span>{'->'}</span></div>
         </div>
     </div>
