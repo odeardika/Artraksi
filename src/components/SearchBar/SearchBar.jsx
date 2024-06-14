6f0d7d8cf758d8084a487188af499bf0f807e42a
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2024-06-14 19:46:55 +0800

    feat : get the event from server

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import styles from './SearchBar.module.css';
 import searchIcon from '../../assets/icon/search.svg'
 
-export default function SearchBar({customStyles}) {
+export default function SearchBar({customStyles, placeholder = "Cari artikel"}) {
   return (
     <>
         <form className={`${styles.search_bar} ${customStyles}`} action="">
-          <input type="text" placeholder="Cari artikel" />
+          <input type="text" placeholder={placeholder} />
           <button><img src={searchIcon} alt="" /></button>
         </form>
     </>
